2e10b87228fdcebc8eea053e2491dad2c989b132
rambabu1458 <58924997+rambabu1458@users.noreply.github.com>
2025-03-01 11:49:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 function insert_Row() {
     //Write your code here
-  
+ //    let task = document.querySelector("#sampleTable");
+	// let tr = document.createElement("tr");
+	
+	// ["NewCell", "NewCell"].forEach(cellText => {
+	//     let td = document.createElement("td");
+	//     td.innerText = cellText;
+	//     tr.appendChild(td);
+	// });
+	
+	// task.appendChild(tr);
+
+	
+let task = document.querySelector("#sampleTable");
+let tr = document.createElement("tr");
+
+let td1 = document.createElement("td");
+td1.innerText = "NewCell";
+tr.appendChild(td1);
+
+let td2 = document.createElement("td");
+td2.innerText = "NewCell";
+tr.appendChild(td2);
+
+// task.appendChild(tr);
+	task.prepend(tr); //it is adding at top
+
   
 }
